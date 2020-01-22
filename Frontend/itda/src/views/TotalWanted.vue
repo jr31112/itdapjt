47fdad023dc97f8733102cb2f81be25497ddaf3e
sungmingyu <0713mingyu@naver.com>
2020-01-22 17:40:55 +0900

    calendar-version1

--- Frontend/itda/src/views/TotalWanted.vue
+++ Frontend/itda/src/views/TotalWanted.vue
@@ -1,8 +1,10 @@
 <template>
-    <v-row class="CalendarSize Center">
+    <v-layout class="CalendarSize Center">
         <v-col>
-            <v-sheet height="64">
-                
+            <v-sheet height = "40">
+                <h2>Hello</h2>
+            </v-sheet>
+            <v-sheet height="64">   
                 <v-toolbar class="Center" flat="flat" color="white" >
                     <v-btn outlined="outlined" class="mr-4" color="grey darken-2" @click="setToday">
                         Today
@@ -25,18 +27,18 @@
                     </v-menu>
                 </v-toolbar>
             </v-sheet>
-            <v-sheet height="600">
+            
+          
+            <v-sheet>
                 <v-calendar
                     ref="calendar"
                     v-model="focus"
                     color="primary"
                     :events="events"
+                    :event-more="false"
                     :event-color="getEventColor"
                     :now="today"
-                    :type="type"
-                    @click:event="showEvent"
-                    @click:more="viewDay"
-                    @click:date="viewDay"
+                    :type="type"                 
                     @change="updateRange"></v-calendar>
                 <v-menu
                     v-model="selectedOpen"
@@ -44,32 +46,14 @@
                     :activator="selectedElement"
                     offset-x="offset-x">
                     <v-card color="grey lighten-4" min-width="350px" flat="flat">
-                        <v-toolbar :color="selectedEvent.color" dark="dark">
-                            <v-btn icon="icon">
-                                <v-icon>mdi-pencil</v-icon>
-                            </v-btn>
-                            <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-                            <v-spacer></v-spacer>
-                            <v-btn icon="icon">
-                                <v-icon>mdi-heart</v-icon>
-                            </v-btn>
-                            <v-btn icon="icon">
-                                <v-icon>mdi-dots-vertical</v-icon>
-                            </v-btn>
-                        </v-toolbar>
-                        <v-card-text>
-                            <span v-html="selectedEvent.details"></span>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-btn text="text" color="secondary" @click="selectedOpen = false">
-                                Cancel
-                            </v-btn>
-                        </v-card-actions>
+                      
+                        
                     </v-card>
                 </v-menu>
             </v-sheet>
+            
         </v-col>
-    </v-row>
+    </v-layout>
 </template>
 
 <script>
@@ -167,77 +151,39 @@
                     .calendar
                     .next()
             },
-            showEvent({nativeEvent, event}) {
-                const open = () => {
-                    this.selectedEvent = event
-                    this.selectedElement = nativeEvent.target
-                    setTimeout(() => this.selectedOpen = true, 10)
-                }
-
-                if (this.selectedOpen) {
-                    this.selectedOpen = false
-                    setTimeout(open, 10)
-                } else {
-                    open()
-                }
-
-                nativeEvent.stopPropagation()
-            },
+           
             updateRange({start, end}) {
+                
                 const events = []
 
-                const min = new Date(`${start.date}T00:00:00`)
-                const max = new Date(`${end.date}T23:59:59`)
-                const days = (max.getTime() - min.getTime()) / 86400000
-                const eventCount = this.rnd(days, days + 20)
-
-                for (let i = 0; i < eventCount; i++) {
-                    const allDay = this.rnd(0, 3) === 0
-                    const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-                    const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-                    const secondTimestamp = this.rnd(
-                        2,
-                        allDay
-                            ? 288
-                            : 8
-                    ) * 900000
-                    const second = new Date(first.getTime() + secondTimestamp)
-
-                    events.push({
-                        name: this.names[this.rnd(0, this.names.length - 1)],
-                        start: this.formatDate(first, !allDay),
-                        end: this.formatDate(second, !allDay),
-                        color: this.colors[this.rnd(0, this.colors.length - 1)]
-                    })
-                }
+                events.push({
+                    name: "안녕 정원",
+                    start: "2020-01-23",
+                    end:"2020-01-25",
+                    color:'blue'
+                }),
+                   events.push({
+                    name: "안녕 정원",
+                    start: "2020-01-23",
+                    end:"2020-01-23",
+                    color:'red'
+                }),
+                  events.push({
+                    name: "안녕 정원",
+                    start: "2020-01-23",
+                    end:"2020-01-23",
+                    color:'yellow'
+                }),
+                  events.push({
+                    name: "안녕 정원",
+                    start: "2020-01-23",
+                    end:"2020-01-23",
+                    color:'green'
+                })
 
                 this.start = start
                 this.end = end
                 this.events = events
-            },
-            nth(d) {
-                return d > 3 && d < 21
-                    ? 'th'
-                    : [
-                        'th',
-                        'st',
-                        'nd',
-                        'rd',
-                        'th',
-                        'th',
-                        'th',
-                        'th',
-                        'th',
-                        'th'
-                    ][d % 10]
-            },
-            rnd(a, b) {
-                return Math.floor((b - a + 1) * Math.random()) + a
-            },
-            formatDate(a, withTime) {
-                return withTime
-                    ? `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()} ${a.getHours()}:${a.getMinutes()}`
-                    : `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()}`
             }
         }
     }

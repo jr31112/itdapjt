7c96cbbbe9c0b214d790c1a6341822adc903555b
김인성 <jr435@naver.com>
2020-01-23 15:11:50 +0900

    WantedDetail - companyinfo

---
+++ Frontend/itda/src/components/DetailCompanyInfo.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container class="detailcompanyinfo my-2 white">
+    <v-row justify="center">
+      <v-col cols="2" align-self="center">
+        
+          <v-card width="150" height="150">
+            <v-img :src="company.logo" :alt="company.corpNm" width="150" aspect-ratio="1" :contain="true">
+            </v-img>
+          </v-card>
+       
+      </v-col>
+      <v-col cols="10" justify="center">
+        <h1>{{company.corpNm}}</h1>
+        <br>
+        <p>홈페이지 : <a :href="company.homePg">{{company.homePg}}</a></p>
+        <p>기업주소 : {{company.corpAddr}}</p>
+        <p>주요사업 : {{company.busiCont}}</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-card class="mx-0">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title two-line>
+                {{company.busiSize}}
+              </v-list-item-title>
+              <v-list-item-subtitle>기업형태</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card class="mx-0">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title two-line>
+                {{company.totPsncnt}}
+              </v-list-item-title>
+              <v-list-item-subtitle>사원수</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card class="mx-0">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title two-line>
+                {{company.yrSalesAmt}}
+              </v-list-item-title>
+              <v-list-item-subtitle>매출</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: "detailcompanyinfo",
+    props:{
+      company:{type:Object}
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Frontend/itda/src/components/DetailWantedInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row class="detailwantedinfo my-2">
+      {{wanted}}
+      {{jobs}}
+  </v-row>
+</template>
+
+<script>
+export default {
+    name: "detailwantedinfo",
+    props:{
+      wanted:{type:Object},
+      jobs:{type:Array}
+    }
+}
+</script>
+
+<style>
+
+</style>

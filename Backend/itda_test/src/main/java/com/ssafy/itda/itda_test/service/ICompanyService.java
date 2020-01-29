fe653b4509fcf197990ec09130af1da9729918b0
banggeut01 <genie121110@gmail.com>
2020-01-29 17:31:50 +0900

    Backend | API 구현 | 직무 CUD

--- Backend/itda_test/src/main/java/com/ssafy/itda/itda_test/service/ICompanyService.java
+++ Backend/itda_test/src/main/java/com/ssafy/itda/itda_test/service/ICompanyService.java
@@ -3,9 +3,9 @@ package com.ssafy.itda.itda_test.service;
 import com.ssafy.itda.itda_test.model.Company;
 
 public interface ICompanyService {
-	public void createCompany(Company company);
+	public void createCompany(Company model);
 	public void deleteCompany(Integer cid);
-	public void updateCompany(Integer cid);
+	public void updateCompany(Company model);
 	
 		
 }

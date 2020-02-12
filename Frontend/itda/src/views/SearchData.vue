698e307f984318c011378830d7451f97888d249d
김인성 <jr435@naver.com>
2020-02-12 00:06:45 +0000

    Update SearchData.vue

--- Frontend/itda/src/views/SearchData.vue
+++ Frontend/itda/src/views/SearchData.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
       <search-data-filter :options="options"/>
-      <search-data-content :options="options" :all="searchresults"  />
-      <!-- 자식으로 부터 받아와서 그걸 보내줘야한다.  -->
+      <search-data-content :options="options" :all="searchresults"/>
   </div>
 </template>
 
@@ -30,21 +29,21 @@ export default {
         getData(){
             var type = this.$route.params.type
             if (type == 1){
-                axios.get('http://54.180.140.163/itda/api/getWantedAll')
+                axios.get('https://192.168.31.54:8197/itda/api/getWantedAll')
                 .then((response)=>{
                         this.searchresults = response.data
                     })
                 .catch()
             }
             else if(type == 2){
-                axios.get('http://54.180.140.163/itda/api/getAllCompanys')
+                axios.get('https://192.168.31.54:8197/itda/api/getAllCompanys')
                     .then((response)=>{
                             this.searchresults = response.data
                         })
                     .catch()
             }
             else if (type==3){
-                axios.get('http://54.180.140.163/itda/api/getAllStacks')
+                axios.get('https://192.168.31.54:8197/itda/api/getAllStacks')
                     .then((response)=>{
                             this.searchresults = response.data
                         })

95751e92d5ac1ca888f83dafb08c6f80da31db4d
Daerae_Kim <drk0830@naver.com>
2020-02-13 18:34:45 +0900

    S02P13B201-103 | Backend | Firebase Social Login 구현

--- Frontend/itda/src/components/PageHeader/SignUpIn.vue
+++ Frontend/itda/src/components/PageHeader/SignUpIn.vue
@@ -1,117 +1,132 @@
 <template>
-
-    <!-- 컨테이너 크기를 500으로해 해주고, fill-heigt를 통해서아래 align center를 이용할 수 있게 됨. -->
-    <v-layout wrap="no wrap">
-      
-      <v-flex xs12="xs12">
-        <v-alert class="mb-1" :value="reChk" type="error">아이디와 비밀번호를 확인해주세요.</v-alert>
-        <v-alert class="mb-1" v-model="isPwError" :value="isPwError" type="error">이메일 형식, 비밀번호를 확인해주세요.</v-alert>
-        <v-alert class="mb-1" v-model="isFormError" :value="isFormError" type="error">입력 값을 모두 넣어주세요.</v-alert>
-        <v-alert class="mb-1" v-model="isRegisterError" :value="isRegisterError" type="error">이미 존재하는 회원의 ID 입니다.</v-alert>
-        <v-alert class="mb-1" :value="isCong" type="success">회원가입을 축하합니다! 로그인 해주세요.</v-alert>
-        <v-card v-if="isChangeLoginRegi">
-          
-          <v-toolbar flat="flat">
-            <v-toolbar-title>Register</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon dark @click="$emit('statusControl',false)">
+  <!-- 컨테이너 크기를 500으로해 해주고, fill-heigt를 통해서아래 align center를 이용할 수 있게 됨. -->
+  <v-layout wrap="no wrap">
+    <v-flex xs12="xs12">
+      <v-alert class="mb-1" :value="reChk" type="error">아이디와 비밀번호를 확인해주세요.</v-alert>
+      <v-alert
+        class="mb-1"
+        v-model="isPwError"
+        :value="isPwError"
+        type="error"
+      >이메일 형식, 비밀번호를 확인해주세요.</v-alert>
+      <v-alert class="mb-1" v-model="isFormError" :value="isFormError" type="error">입력 값을 모두 넣어주세요.</v-alert>
+      <v-alert
+        class="mb-1"
+        v-model="isRegisterError"
+        :value="isRegisterError"
+        type="error"
+      >이미 존재하는 회원의 ID 입니다.</v-alert>
+      <v-alert class="mb-1" :value="isCong" type="success">회원가입을 축하합니다! 로그인 해주세요.</v-alert>
+      <v-card v-if="isChangeLoginRegi">
+        <v-toolbar flat="flat">
+          <v-toolbar-title>Register</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon dark @click="$emit('statusControl',false)">
             <v-icon color="black">mdi-close</v-icon>
-            </v-btn>
-            <!-- <v-btn color="green darken-1" text="text" @click="dialog = false">Close</v-btn> -->
-          </v-toolbar>
-          <div class="pa-4" style="max-width: 540px" name="regi">
-            <v-text-field v-model="name" :value ="name" label="Name*"></v-text-field>
-            <v-text-field v-model="email_rg" :value ="email_rg" label="Email*"></v-text-field>
-            <v-row>
-              <v-col cols="6">
-                <v-text-field
-                  v-model= "password_rg"
-                  :value ="password_rg"
-                  type="password"
-                  label="password*"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="6">
-                <v-text-field
-                  v-model= "password_rg1"
-                  :value ="password_rg1"
-                  type="password"
-                  label="password check*"
-                  @keyup.enter="goRegister()"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-              <v-layout>
-                <v-btn
-                  class="mb-1"
-                  color="primary"
-                  depressed="depressed"
-                  block="block"
-                  large="large"
-                  @click="goRegister()"
-                >Save</v-btn>
-              </v-layout>
-              <v-layout>
-                <v-btn
-                  class="mb-1"
-                  depressed="depressed"
-                  block="block"
-                  large="large"
-                  @click="isChangeLoginRegi = !isChangeLoginRegi; goInit()"
-            
-                >Cancel</v-btn>
-              </v-layout>
-          </div>
-        </v-card>
-        <v-card v-else>
-          <v-toolbar flat="flat">
-            <v-toolbar-title>Login</v-toolbar-title>
-             <v-spacer></v-spacer>
-             <v-btn icon dark @click="$emit('statusControl',false)">
-               <v-icon color="black darken-2">mdi-close</v-icon>
-             </v-btn>
-            <!-- <v-btn color="green darken-1" text="text" @click="$emit.dialog = !$emit.dialog">Close</v-btn> -->
-          </v-toolbar>
-          <div class="pa-4" style="max-width: 540px">
-            <!-- pa는 패딩오토 -->
-            <v-text-field v-model="email_lg"  label="email을 입력하세요"></v-text-field>
-            <v-text-field
-              v-model="password_lg"
-              
-              type="password"
-              label="password를 입력하세요"
-              @keyup.enter="goLogin()"
-            ></v-text-field>
+          </v-btn>
+          <!-- <v-btn color="green darken-1" text="text" @click="dialog = false">Close</v-btn> -->
+        </v-toolbar>
+        <div class="pa-4" style="max-width: 540px" name="regi">
+          <v-text-field v-model="name" :value="name" label="Name*"></v-text-field>
+          <v-text-field v-model="email_rg" :value="email_rg" label="Email*"></v-text-field>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="password_rg"
+                :value="password_rg"
+                type="password"
+                label="password*"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="password_rg1"
+                :value="password_rg1"
+                type="password"
+                label="password check*"
+                @keyup.enter="goRegister()"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-layout>
             <v-btn
               class="mb-1"
               color="primary"
               depressed="depressed"
               block="block"
               large="large"
-              @click="login(
+              @click="goRegister()"
+            >Save</v-btn>
+          </v-layout>
+          <v-layout>
+            <v-btn
+              class="mb-1"
+              depressed="depressed"
+              block="block"
+              large="large"
+              @click="isChangeLoginRegi = !isChangeLoginRegi; goInit()"
+            >Cancel</v-btn>
+          </v-layout>
+        </div>
+      </v-card>
+      <v-card v-else>
+        <v-toolbar flat="flat">
+          <v-toolbar-title>Login</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon dark @click="$emit('statusControl',false)">
+            <v-icon color="black darken-2">mdi-close</v-icon>
+          </v-btn>
+          <!-- <v-btn color="green darken-1" text="text" @click="$emit.dialog = !$emit.dialog">Close</v-btn> -->
+        </v-toolbar>
+        <div class="pa-4" style="max-width: 540px">
+          <!-- pa는 패딩오토 -->
+          <v-text-field v-model="email_lg" label="email을 입력하세요"></v-text-field>
+          <v-text-field
+            v-model="password_lg"
+            type="password"
+            label="password를 입력하세요"
+            @keyup.enter="goLogin()"
+          ></v-text-field>
+          <v-btn
+            class="mb-1"
+            color="primary"
+            depressed="depressed"
+            block="block"
+            large="large"
+            @click="login(
               {
                   email: email_lg,
                   pw: password_lg
               }, reChk = !reChk ,
                  isCong= false
               )"
-            >Login</v-btn>
-            <v-btn
-              class="mb-1"
-              depressed="depressed"
-              block="block"
-              large="large"
-              @click="isChangeLoginRegi = !isChangeLoginRegi; goInit('isChangeLoginRegi')"
-            >Register</v-btn>
-          </div>
-        </v-card>
-      </v-flex>
-    </v-layout>
+          >Login</v-btn>
+          <!-- social login -->
+          <v-btn
+            class="mb-1"
+            color="#dd4b39"
+            dark
+            depressed="depressed"
+            block="block"
+            large="large"
+            @click="socialLogin()"
+          >Google Login</v-btn>
+          <v-btn
+            class="mb-1"
+            depressed="depressed"
+            block="block"
+            large="large"
+            @click="isChangeLoginRegi = !isChangeLoginRegi; goInit('isChangeLoginRegi')"
+          >Register</v-btn>
+        </div>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import { mapState, mapActions } from "vuex";
 import axios from "axios";
-import firebase from 'firebase';
+import firebase from "firebase";
 
 export default {
   data() {
@@ -143,22 +158,20 @@ export default {
         this.name == null ||
         this.email_rg == null ||
         this.password_rg == null ||
-        this.password_rg1 == null ) 
-      {
-        
+        this.password_rg1 == null
+      ) {
         this.isFormError = true;
         this.isResisterError = false;
-        this.isPwError =false;
-      }
-      else if(this.email_rg.indexOf("@")== -1 ||
-      this.email_rg.indexOf("@")== 0 ||
-      this.email_rg.length === this.email_rg.indexOf("@")+1 ||
-      this.password_rg !== this.password_rg1
-      ) 
-      {
-         this.isFormError = false;
-         this.isResisterError = false;
-         this.isPwError =true;
+        this.isPwError = false;
+      } else if (
+        this.email_rg.indexOf("@") == -1 ||
+        this.email_rg.indexOf("@") == 0 ||
+        this.email_rg.length === this.email_rg.indexOf("@") + 1 ||
+        this.password_rg !== this.password_rg1
+      ) {
+        this.isFormError = false;
+        this.isResisterError = false;
+        this.isPwError = true;
       }
       ////////////////이부분의 처리 필요
       else {
@@ -173,13 +186,11 @@ export default {
               this.isChangeLoginRegi = !this.isChangeLoginRegi;
               this.isFormError = false;
               this.isRegisterError = false;
-              this.isPwError =false;
-              
+              this.isPwError = false;
+
               this.isCong = !this.isCong;
-            } 
-            else 
-            {
-              this.isPwError=false;
+            } else {
+              this.isPwError = false;
               this.isFormError = false;
               this.isRegisterError = true;
               return;
@@ -191,25 +202,21 @@ export default {
       }
     },
     ...mapActions(["login", "register"]),
-    goInit()
-    {
+    goInit() {
       //제출 후 초기화.
-      this.email_lg=null, 
-      this.password_lg= null,
-      this.name =null, 
-      this.email_rg =null,
-      this.password_rg =null,
-      this.password_rg1 =null 
-      if(this.isChangeLoginRegi)
-      {
-        this.reChk = false; 
+      (this.email_lg = null),
+        (this.password_lg = null),
+        (this.name = null),
+        (this.email_rg = null),
+        (this.password_rg = null),
+        (this.password_rg1 = null);
+      if (this.isChangeLoginRegi) {
+        this.reChk = false;
         this.isCong = false;
-      }
-      else
-      {
-        this.isRegisterError =false; 
-        this.isPwError= false; 
-        this.isFormError =false;
+      } else {
+        this.isRegisterError = false;
+        this.isPwError = false;
+        this.isFormError = false;
       }
     },
     goLogin() {
@@ -218,60 +225,73 @@ export default {
         pw: this.password_lg
       });
     },
-    socialLogin(){
+    socialLogin() {
       const provider = new firebase.auth.GoogleAuthProvider();
 
-      firebase.auth().signInWithPopup(provider)
-        .then(result =>{
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => {
           var social_email = result.user.email;
           var social_uid = result.user.uid;
           var social_name = result.user.displayName;
           console.log(result);
-          console.log(social_email)
-          console.log(social_uid)
+          console.log(social_email);
+          console.log(social_uid);
           axios
-            .get('http://192.168.31.54:8197/itda/api/emailCheck/'+ social_email)
+            .get(
+              "http://192.168.31.54:8197/itda/api/emailCheck/" + social_email
+            )
             .then(response => {
-              if(response.data.state == 'fail'){
+              if (response.data.state == "fail") {
                 //이미 있는 email이라면?
                 axios
-                  .post('http://192.168.31.54:8197/itda/api/login', {email : social_email, pw: social_uid})
+                  .post("http://192.168.31.54:8197/itda/api/login", {
+                    email: social_email,
+                    pw: social_uid
+                  })
                   .then(response => {
-                    if(response.data.state == 'fail'){
+                    if (response.data.state == "fail") {
                       //로그인 실패면 이미 해당 이메일로 회원가입한 적이 있을 것!
-                      alert('해당 email로 가입한 적이 있습니다!')
+                      alert("해당 email로 가입한 적이 있습니다!");
                       return;
-                    }else{
-                      let token = response.headers['jwt-auth-token']
-                      localStorage.setItem("access_token", token)
-                      localStorage.setItem("social", "social")
+                    } else {
+                      let token = response.headers["jwt-auth-token"];
+                      localStorage.setItem("access_token", token);
+                      localStorage.setItem("social", "social");
                       this.$router.go();
                     }
-                  })
-              }
-              else{
+                  });
+              } else {
                 //회원가입 시킨 뒤 로그인 시키자.
                 axios
-                  .post('http://192.168.31.54:8197/itda/api/signUp', {email: social_email, pw:social_uid, uname:social_name})
-                  .then(response =>{
-                    if(response.data.state == 'success'){
-                       axios
-                        .post('http://192.168.31.54:8197/itda/api/login', {email : social_email, pw: social_uid})
+                  .post("http://192.168.31.54:8197/itda/api/signUp", {
+                    email: social_email,
+                    pw: social_uid,
+                    uname: social_name
+                  })
+                  .then(response => {
+                    if (response.data.state == "success") {
+                      axios
+                        .post("http://192.168.31.54:8197/itda/api/login", {
+                          email: social_email,
+                          pw: social_uid
+                        })
                         .then(response => {
-                          if(response.data.state == 'success'){
-                            let token = response.headers['jwt-auth-token']
-                            localStorage.setItem("access_token", token)
-                            localStorage.setItem("social", "social")
+                          if (response.data.state == "success") {
+                            let token = response.headers["jwt-auth-token"];
+                            localStorage.setItem("access_token", token);
+                            localStorage.setItem("social", "social");
                             this.$router.go();
                           }
-                        })
+                        });
                     }
-                  })
+                  });
               }
-            })
+            });
         })
-        .catch((err)=>{
-          alert("Google Login이 정상적으로 실행되지 않았습니다!" + err.message)
+        .catch(err => {
+          alert("Google Login이 정상적으로 실행되지 않았습니다!" + err.message);
         });
     }
   }

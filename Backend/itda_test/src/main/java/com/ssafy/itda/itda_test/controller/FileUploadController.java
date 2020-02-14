50767314cdf59e208550f495cb3ea21ee2167af4
Daerae_Kim <drk0830@naver.com>
2020-02-14 17:58:08 +0900

    S02P13B201-106 | Backend | API 수정 - 파일 업로드 관련

--- Backend/itda_test/src/main/java/com/ssafy/itda/itda_test/controller/FileUploadController.java
+++ Backend/itda_test/src/main/java/com/ssafy/itda/itda_test/controller/FileUploadController.java
@@ -1,21 +1,15 @@
 package com.ssafy.itda.itda_test.controller;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,58 +17,50 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ssafy.itda.itda_test.exception.FileUploadException;
+import com.ssafy.itda.itda_test.model.User;
 import com.ssafy.itda.itda_test.payload.FileUploadResponse;
 import com.ssafy.itda.itda_test.service.FileUploadDownloadService;
+import com.ssafy.itda.itda_test.service.IUserService;
+import com.ssafy.itda.itda_test.service.JwtServiceImpl;
 
 @RequestMapping("/api")
 @RestController
 public class FileUploadController {
 	private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
-	
+
 	@Autowired
 	private FileUploadDownloadService service;
-	
+
+	@Autowired
+	private IUserService userService;
+
+	@Autowired
+	private JwtServiceImpl jwtService;
+
 	@PostMapping("/uploadFile")
-	public FileUploadResponse uploadFile(@RequestParam("file") MultipartFile file) {
-        String fileName = service.storeFile(file);
-        
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                                .path("/downloadFile/")
-                                .path(fileName)
-                                .toUriString();
-        
-        return new FileUploadResponse(fileName, fileDownloadUri, file.getContentType(), file.getSize());
-    }
-    
-//    @PostMapping("/uploadMultipleFiles")
-//    public List<FileUploadResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files){
-//        return Arrays.asList(files)
-//                .stream()
-//                .map(file -> uploadFile(file))
-//                .collect(Collectors.toList());
-//    }
-    
-    @GetMapping("/downloadFile/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request){
-         // Load file as Resource
-        Resource resource = service.loadFileAsResource(fileName);
- 
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
-        }
- 
-        // Fallback to the default content type if type could not be determined
-        if(contentType == null) {
-            contentType = "application/octet-stream";
-        }
- 
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
-    }
+	public FileUploadResponse uploadFile(@RequestParam("file") MultipartFile file, HttpServletRequest req) {
+		String fileName = service.storeFile(file);
+		String fileDownloadUri = null;
+		Map<String, Object> resultMap = new HashMap<>();
+		String token = req.getHeader("jwt-auth-token");
+		if (token != null && !token.equals("")) {
+			resultMap.putAll(jwtService.get(token));
+			int uid = (int) resultMap.get("uid");
+			User user = userService.getUser(uid);
+			if (user.getUimg() != null) {
+				File uimg_file = new File(user.getUimg());
+				if (uimg_file.exists()) {
+					uimg_file.delete();
+				}
+			}
+			fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/images/").path(fileName)
+					.toUriString();
+			user.setUimg(fileDownloadUri);
+			userService.updateUser(user);
+			return new FileUploadResponse(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+		} else {
+			return new FileUploadResponse(null, null, null, 0L);
+		}
+	}
 }

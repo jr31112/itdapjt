e3174c679a845e0721db6ff22305455e80a73b5a
banggeut01 <genie121110@gmail.com>
2020-02-19 13:35:15 +0900

    S02P13B201-130 | Backend | API - Add Comment in StudyDetail Page

---
+++ Backend/itda_test/src/main/java/com/ssafy/itda/itda_test/model/Comment.java
@@ -0,0 +1,69 @@
+package com.ssafy.itda.itda_test.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Comment implements Serializable {
+	private int cmid;
+	private int uid;
+	private int stid;
+	private String content;
+	private Timestamp createdAt;
+	private Timestamp updatedAt;
+	
+	public Comment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Comment(int cmid, int uid, int stid, String content, Timestamp createdAt, Timestamp updatedAt) {
+		super();
+		this.cmid = cmid;
+		this.uid = uid;
+		this.stid = stid;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+	public int getCmid() {
+		return cmid;
+	}
+	public void setCmid(int cmid) {
+		this.cmid = cmid;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public int getStid() {
+		return stid;
+	}
+	public void setStid(int stid) {
+		this.stid = stid;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	@Override
+	public String toString() {
+		return "Comment [cmid=" + cmid + ", uid=" + uid + ", stid=" + stid + ", content=" + content + ", createdAt="
+				+ createdAt + ", updatedAt=" + updatedAt + "]";
+	}
+
+}
---
+++ Backend/itda_test/src/main/java/com/ssafy/itda/itda_test/service/CommentServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ssafy.itda.itda_test.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.itda.itda_test.dao.CommentDao;
+import com.ssafy.itda.itda_test.model.Comment;
+
+@Service
+public class CommentServiceImpl implements ICommentService {
+	
+	@Autowired
+	private CommentDao commentDao;
+	
+	@Override
+	public List<Comment> getComments(int stid) {
+		return commentDao.getComments(stid);
+	}
+	
+	@Override
+	public void createComment(Comment model) {
+		commentDao.createComment(model);
+	}
+	
+	@Override
+	public void updateComment(Comment model) {
+		commentDao.updateComment(model);
+	}
+	
+	@Override
+	public void deleteComment(int cmid) {
+		commentDao.deleteComment(cmid);
+	}
+}

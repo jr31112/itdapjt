89b64263d9853437cb54796e0cf50b35b9c4edef
김인성 <jr435@naver.com>
2020-02-19 15:21:57 +0900

    ADD:studyDetailPage

--- Frontend/itda/src/views/StudyDetail.vue
+++ Frontend/itda/src/views/StudyDetail.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="studydetail">
-
+      {{study}}
+      {{person}}
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name:"stduydetail",
-    methods:{
-
+    data(){
+        return{
+            person:[],
+            study:{}
+        }
     },
-    watch:{
-        
-    }
+    methods:{
+        getStudy(){
+            axios.get('https://i02b201.p.ssafy.io:8197/itda/api/getStudy/'+this.$route.params.id)
+                .then(response => {
+                    this.study = response.data
+                })
+        },
+        getPerson(){
+            axios.get('https://i02b201.p.ssafy.io:8197/itda/api/getStudyGroup/'+this.$route.params.id)
+              .then(response => {
+                  this.person = response.data
+          })
+        },
+    },
+    mounted(){
+        this.getStudy()
+        this.getPerson()
+    },
 }
 </script>
 

c95e4c22c5e33c42609504da6e4ef87ad2e7fc61
sungmingyu <0713mingyu@naver.com>
2020-02-20 11:40:10 +0900

    ADD: StudyDetailPage

--- Frontend/itda/src/views/StudyDetail.vue
+++ Frontend/itda/src/views/StudyDetail.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="studydetail">
-      {{study}}
-      {{person}}
+      <div>
+      <study-detail-page :study="study" :person="person"/>
+      </div>
+
+      <div>
+          <!-- 댓글 -->
+      </div>
+    <div>
+        <!-- 파일 업로드 -->
+    </div>
   </div>
+  
 </template>
 
 <script>
 import axios from 'axios'
-
+import StudyDetailPage from '../components/StudyMain/StudyLoginContent/StudyDetailPage.vue'
 export default {
     name:"stduydetail",
     data(){
@@ -15,6 +24,9 @@ export default {
             person:[],
             study:{}
         }
+    },
+    components:{
+        StudyDetailPage
     },
     methods:{
         getStudy(){
